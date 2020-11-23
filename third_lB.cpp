0cc2252c7dd5f1740c59a940bc0948f999a7dc4d
anastainska <anastasia.kajinska@gmail.com>
2020-11-23 09:58:10 +0200

    my first commit

---
+++ third_lB.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <math.h>
+
+int NumberOfDigits(int A)
+{
+	return log10(A) + 1;
+}
+
+void main()
+{
+	int Input;
+	printf("Enter your number");
+	scanf_s("%d", &Input);
+	int Output = NumberOfDigits(Input);
+	printf("The number of digits in your number is %i", Output);
+}
